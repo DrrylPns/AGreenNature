16db10530a57ef587386227d3927a5fa79046010
Ralph Saladino <130468937+hplar05@users.noreply.github.com>
2023-10-10 21:49:19 +0800

    Adding div border.
    Adding div border.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import { Kaisei_Decol } from "next/font/google";
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>Initial Setup</h1>;
+  return (
+    <div className="container">
+      <h1 className="bg-red-100">Testing</h1>
+    </div>
+  );
 }
